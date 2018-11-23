021436fc75d7787ab7a52b17116b8c265234a585
Lubin1611 <43751457+Lubin1611@users.noreply.github.com>
2018-11-23 14:37:20 +0100

    Tri v2

--- java.js
+++ java.js
@@ -1,35 +1,125 @@
-var srcdechet = ["conserve.png", "bouteille.jpg", "pile.jpg", "journal.jpg"];
+var tousLesid = ["conserve", "bouteille", "journal", "pile", "restes", "bouteillevin", "magazine", "coca", "pastis", "potyaourt"];
 
-var rdmSrc = srcdechet[Math.floor(Math.random() * srcdechet.length)];
+var support = document.getElementById("dechetAlea");
 
+var rdmId = tousLesid[Math.floor(Math.random() * tousLesid.length)];
 
-var imgDechet = document.createElement("img");
-imgDechet.src = "bouteille.jpg";
-imgDechet.id = "bouteille";
-imgDechet.style.width = "100px";
-imgDechet.style.height = "100px";
-document.getElementById("liste_dechets").appendChild(imgDechet);
+var bonneRep = 0;
+var essais = 0;
 
+support.id = rdmId;
 
 
 
-  function verifDechet() {
+document.getElementById("boutonJaune").addEventListener("click", function () {
 
-      if (imgDechet.id == "bouteille") {
+    if (support.id == "conserve" || support.id == "bouteille" || support.id == "coca") {
 
-          alert("Bonne réponse");
-          document.getElementById("nextButton").innerHTML = "<button onclick = nextBtn() id = \"placement\">Suivant</button>";
+        essais++;
 
-      }
-  }
+        alert("Bonne réponse, vous avez placé le déchet dans la bonne poubelle");
+        document.getElementById("nextButton").innerHTML = "<button onclick = nextBtn()>Suivant</button>";
+        console.log(bonneRep);
+        console.log(essais);
 
+    }  else  {
+
+
+        document.getElementById("nextButton").innerHTML = "<button onclick = nextBtn()>Suivant</button>";
+        alert("Mauvaise réponse, ce n'est pas la bonne poubelle");
+        console.log(bonneRep);
+        console.log(essais);
+
+    }
+});
+
+
+
+document.getElementById("boutonBleu").addEventListener("click", function () {
+
+    if (support.id == "journal" || support.id == "magazine") {
+
+        essais++;
+
+        alert("Bonne réponse, vous avez placé le déchet dans la bonne poubelle");
+        document.getElementById("nextButton").innerHTML = "<button onclick = nextBtn()>Suivant</button>";
+        console.log(bonneRep);
+        console.log(essais);
+
+    }  else {
+
+        document.getElementById("nextButton").innerHTML = "<button onclick = nextBtn()>Suivant</button>";
+        alert("Mauvaise réponse, ce n'est pas la bonne poubelle");
+        console.log(bonneRep);
+        console.log(essais);
+    }
+
+});
+
+
+
+
+document.getElementById("boutonVert").addEventListener("click", function () {
+
+    if (support.id == "bouteillevin" || support.id == "pastis") {
+
+        essais++;
+
+        alert("Bonne réponse, vous avez placé le déchet dans la bonne poubelle");
+        document.getElementById("nextButton").innerHTML = "<button onclick = nextBtn()>Suivant</button>";
+        console.log(bonneRep);
+        console.log(essais);
+
+    }  else  {
+
+
+
+        document.getElementById("nextButton").innerHTML = "<button onclick = nextBtn()>Suivant</button>";
+        alert("Mauvaise réponse, ce n'est pas la bonne poubelle");
+        console.log(bonneRep);
+        console.log(essais);
+    }
+});
+
+
+
+document.getElementById("boutonMarron").addEventListener("click", function () {
+
+    if (support.id == "pile" || support.id == "restes" || support.id == "potyaourt") {
+
+        essais++;
+
+        alert("Bonne réponse, vous avez placé le déchet dans la bonne poubelle");
+        document.getElementById("nextButton").innerHTML = "<button onclick = nextBtn()>Suivant</button>";
+        console.log(bonneRep);
+        console.log(essais);
+
+    }  else  {
+
+
+        document.getElementById("nextButton").innerHTML = "<button onclick = nextBtn()>Suivant</button>";
+        alert("Mauvaise réponse, ce n'est pas la bonne poubelle");
+        console.log(bonneRep);
+        console.log(essais);
+
+    }
+
+    if (essais == 3) {
+
+        alert("Bien joué, vous avez réussi a ranger 10 déchets correctement ! Appuyer sur Ok pour voir votre score");
+        document.getElementById("containerJeu").style.display = "none";
+    }
+
+});
 
 
 
 function nextBtn() {
 
-    document.getElementById("nextButton").innerHTML = "";
-    imgDechet.src = rdmSrc;
+    rdmId = [Math.floor(Math.random() * tousLesid.length)];
+    document.getElementById("nextButton").innerHTML = " ";
+    support.id = tousLesid[rdmId];
+
 }
 
-console.log(srcdechet);
+
